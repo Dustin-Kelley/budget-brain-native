97aaa29afb568cc29779b16b8fb15dd7aefcf7a4
Dustin Kelley <Kelleydustin2018@gmail.com>
2026-02-20 08:36:02 -0600

    feat: add component library

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,12 +1,18 @@
-import { Text, View } from "react-native";
+import { Button } from "@/components/ui/button";
+import { Text } from "@/components/ui/text";
+import { View } from "react-native";
 
 export default function OverviewScreen() {
   return (
 
-    <View className="flex-1 items-center justify-center bg-white">
+    <View className="flex-1 items-center justify-center bg-white h-full">
       <Text className="text-xl font-bold text-red-500">
-        Welcome to Nativewind!
+        Welcome
       </Text>
+      <Button>
+        <Text variant="large">Click me hello</Text>
+      </Button>
+      <Text variant="h1">Click me hello</Text>
     </View>
 
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,35 @@ module.exports = {
   content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        border: '#e2e8f0',
+        input: '#e2e8f0',
+        ring: '#94a3b8',
+        background: '#ffffff',
+        foreground: '#0f172a',
+        primary: {
+          DEFAULT: '#0f172a',
+          foreground: '#f8fafc',
+        },
+        secondary: {
+          DEFAULT: '#f1f5f9',
+          foreground: '#0f172a',
+        },
+        destructive: {
+          DEFAULT: '#ef4444',
+          foreground: '#f8fafc',
+        },
+        muted: {
+          DEFAULT: '#f1f5f9',
+          foreground: '#64748b',
+        },
+        accent: {
+          DEFAULT: '#f1f5f9',
+          foreground: '#0f172a',
+        },
+      },
+    },
   },
   plugins: [],
 };
