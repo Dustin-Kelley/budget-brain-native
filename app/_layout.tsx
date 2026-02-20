5747d6355b7080b6c891bab23681e477cdd88fcd
Dustin Kelley <Kelleydustin2018@gmail.com>
2026-02-20 08:48:14 -0600

    feat: install react query

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,19 @@
 import "../global.css";
 import { AuthProvider } from "@/contexts/auth-context";
+import { queryClient } from "@/lib/query-client";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
-
 
 export default function RootLayout() {
   return (
-    <AuthProvider>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" />
-        <Stack.Screen name="(auth)" />
-        <Stack.Screen name="(app)" />
-      </Stack>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="index" />
+          <Stack.Screen name="(auth)" />
+          <Stack.Screen name="(app)" />
+        </Stack>
+      </AuthProvider>
+    </QueryClientProvider>
   );
 }
